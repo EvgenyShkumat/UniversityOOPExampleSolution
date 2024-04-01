19b44607e480c4ac5425693b2367061f28724f1f
Temashevk <a.shevko2008@gmail.com>
2024-04-01 18:16:34 +0300

    Add new way to create objets

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -2,19 +2,19 @@
 #include "Student.h"
 
 int main() {
-	Student st1("Alex");
-	Student st2("Potter");
-	Student st3("Alice");
+	Student st1 = Student("Alex");
+	Student st2("Potter", 14);
+	Student st3("Alice", 15, 10);
 	Student st4("Peter");
 
-	st1.age = 14;
+	/*st1.age = 14;
 	st1.mark = 9;
 
 	st2.age = 16;
 	st2.mark = 10;
 
 	st3.age = 13;
-	st3.mark = 7;
+	st3.mark = 7;*/
 
 	cout << st1.convert() << endl;
 	cout << st2.convert() << endl;

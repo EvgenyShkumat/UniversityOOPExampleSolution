41e4ebbd410c7dfbe9bf2285dfc98f523cb461e7
Temashevk <a.shevko2008@gmail.com>
2024-04-08 18:38:17 +0300

    use Incapsulation in Student class

--- Project01/Main.cpp
+++ Project01/Main.cpp
@@ -3,26 +3,7 @@
 
 int main() {
 	
-	Student st1{"Alex", 14, 7};
-	Student st2{"Alice", 14, 5};
-	Student st3{"Roma", 14, 10};
-	Student st4{"Matvey", 14, 9};
-	Student st5{"Alexey", 14, 6};
-
-	int const size = 5;
-
-	Student students[size]{st1, st2, st3, st4, st5};
-	Manager manag;
-
-	cout << "Best mark is " << manag.getMaxMark(students, size) << endl;
-	cout << "Worst mark is " << manag.getMinMark(students, size) << endl;
-
-	Student best = manag.getBestStudent(students, size);
-	Student worst = manag.getWorstStudent(students, size);
-
-	cout << "First best student is " << best.convert() << endl;
-	cout << "First worst student is " << worst.convert() << endl;
-	cout << "Average mark is " << manag.getAvgMark(students, size);
+	Student st1{"Alex", 14, 9};
 	
 
 	return 0;
--- Project01/Student.h
+++ Project01/Student.h
@@ -4,28 +4,26 @@
 using namespace std;
 
 class Student {
-public:
+private:
 	string name;
 	int age;
 	double mark;
 
+public:
+	
 	// default constructor 
-
 	/*Student() {
 		name = "no name";
 		age = 6;
 		mark = 4;
 	}*/
-
 	//Student(string nm) {
 	//	name = nm;
 	//}
-
 	//Student(string nm, int a) {
 	//	name = nm;
 	//	age = a;
 	//}
-
 	//// canonical constructor
 
 	Student(string nm = "no name", int a = 6, double m = 4) {
@@ -50,5 +48,29 @@ public:
 		s += ", mark = " + to_string(mark);
 
 		return s;
+	}
+
+	string getName() {
+		return name;
+	}
+
+	int getAge() {
+		return age;
+	}
+
+	void setAge(int a) {
+		if (a > 0 && a < 100) {
+			age = a;
+		}
+	}
+
+	double getMark() {
+		return mark;
+	}
+
+	void setMark(double m) {
+		if (m >= 0 && m <= 10) {
+			mark = m;
+		}
 	}
 };
